6f8c49de134a194e796895f47432d0d21ca9e628
TheDevGoSike283 <111324250+TheDevGoSike283@users.noreply.github.com>
2023-02-26 22:39:48 -0500

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -7,13 +7,13 @@ q = num2
 rmd = num1 % num2
 quo = (snum - rmd) / q
 print("Steps to solve (with Euclid's division algorithm) Note that the last highlited number is the answer:")
-print(str(snum) + " = \033[33;33m" + str(q) + "\033[0;m x " + str(quo) + " + " + str(rmd))
+print(str(snum) + " = \033[33;33m" + str(q) + "\033[0;m x " + str(round(quo)) + " + " + str(rmd))
 while not rmd == 0:
     snum = q
     q = rmd
     rmd = snum % q
     quo = (snum - rmd) / q
-    print(str(snum) + " = \033[33;33m" + str(q) + "\033[0;m x " + str(quo) + " + " + str(rmd))
+    print(str(snum) + " = \033[33;33m" + str(q) + "\033[0;m x " + str(round(quo)) + " + " + str(rmd))
 
 print("Greatest common factor is " + str(q) + ".")
 
